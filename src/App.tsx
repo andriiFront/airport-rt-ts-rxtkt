463f42323fe7f5ac3a24c4f6414ef56b7fb66f2b
Andrii <badekha@gmail.com>
2022-12-17 22:39:34 +0200

    implemented main page landing

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom'
+import { Navigation } from './components/Navigation';
 import { AirportDetailPage } from './pages/AirportDetailPage';
 import { AuthPage } from './pages/AuthPage';
 import { MainPage } from './pages/MainPage';
 
 function App() {
   return (
-    <div>
+    <>
+      <Navigation />
       <Routes>
         <Route path="/" element={<MainPage/>} />
         <Route path="/auth" element={<AuthPage/>} />
         <Route path="/airport" element={<AirportDetailPage/>} />
       </Routes>
-    </div>
+    </>
   );
 }
 
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,13 @@
+import { AirportCard } from "../components/AirportCard";
+import { AirportFilter } from "../components/AirportFilter";
+import { AirportSearch } from "../components/AirportSearch";
+
 export function MainPage() {
   return (
-    <div>
-      main
+    <div className="container mx-auto max-w-[760px] pt-5">
+      <AirportSearch/>
+      <AirportFilter />
+      <AirportCard />
     </div>
   )
 }
